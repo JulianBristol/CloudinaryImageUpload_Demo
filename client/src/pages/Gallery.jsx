aae71e3636dc7eab1fa28921040dce61f1a1342f
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-02-06 22:46:19 -0500

    Update website gallery page

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
+import { Image } from 'cloudinary-react'
 
 const Gallery = () => {
+  const [imageIds, setImageIds] = useState('');
+
+  const loadImages = async (res, req) =>{
+    try {
+      const res = await fetch('/api/images');
+      const data = await res.json();
+      setImageIds(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    loadImages();
+  },[]);
+
   return (
     <div>
-      testing the Gallery
+      <h1 className="title">Gallery</h1>
+      {imageIds && imageIds.map((imageId, index) => (
+        <Image
+        key={index}
+        cloudName="dfb1oxmpx"
+        publicId={imageId}
+        width="300"
+        crop="scale"
+        borderRadius="10"
+        />
+      ))}
     </div>
   )
 }
