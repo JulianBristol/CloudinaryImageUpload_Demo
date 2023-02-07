b80f4151ad852080bbbe87a07851caefb0954b49
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-02-06 22:24:04 -0500

    Connect backend to Cloudinary website

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # cloudinary-image-upload
 Use Cloudinary to save photos to the cloud and learn how to incorporate it into React projects
 This follows the tutorial from James Q Quick at https://youtu.be/Rw_QeJLnCK4
-Uses React, react-router-dom, cloudinary-react
+Uses React, react-router-dom, cloudinary-react, dotenv
 
 # Getting Started with Create React App
 
--- server/server.js
+++ server/server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const app = express();
 
+const { cloudinary } = require('./utils/cloudinary');
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true}));
 
+app.get('/api/images', async (req, res) => {
+    const { resources } = await cloudinary.search
+        .expression('dev_upload_preset')
+        .sort_by('public_id', 'desc')
+        .max_results(30)
+        .execute();
 
-app.post('/api/upload', (req, res) => {
+    const publicIds = resources.map((file) => file.public_id);
+    res.send(publicIds);
+});
+
+app.post('/api/upload', async(req, res) => {
     try {
         const fileString = req.body.data;
-        
+        const uploadedResponse = await cloudinary.uploader.upload(fileString, {
+            upload_preset: 'dev_upload_preset'
+        })
+        console.log(uploadedResponse);
+        res.json({msg: "Image uploaded successfully"})
     } catch (error) {
         console.error(error);
+        res.status(500).json({err: 'Error uploading image'});
     }
 });
 
